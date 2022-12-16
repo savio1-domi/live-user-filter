9c9d81745e95ee98b8ea4f37c4fb57204e9a9001
git-savio1.domi <techsavvydominic3@gmail.com>
2022-12-16 17:17:57 +0300

    Done Project

---
+++ script.js
@@ -0,0 +1,44 @@
+const result = document.getElementById('result')
+const filter = document.getElementById('filter')
+const listItems = []
+
+getData()
+
+filter.addEventListener('input', (e) => filterData(e.target.value))
+
+async function getData() {
+    const res = await fetch('https://randomuser.me/api?results=200')
+    
+    const { results } = await res.json()
+
+    // clear results
+    result.innerHTML = ''
+
+    results.forEach(user => {
+        const li = document.createElement('LI')
+
+
+        listItems.push(li)
+
+        li.innerHTML =
+        `<img src="${user.picture.large}" alt="${user.name.first}"
+        <div class="user-info">
+            <h4>${user.name.first} ${user.name.last}</h4>
+            <p>${user.location.city}, ${user.location.country}</p>
+        </div>
+        `
+
+        result.appendChild(li)
+    })
+}
+
+function filterData(searchTerm) {
+    listItems.forEach(item => {
+        if(item.innerText.toLowercase().includes(searchTerm.toLowercase())) {
+            item.classList.remove('hide')
+        }else{
+            item.classList.add('hide')
+        }
+    }
+    )
+}
